54a410876905feaa2309029cfcb8d4f24eae7602
mohankumar-markuli <mohankumarmarkuli@gmail.com>
2026-07-04 15:55:08 +0530

    fix: update repository name in next.config.mjs to /portfolio

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
-const repoName = '/mohankumar-markuli';
+const repoName = '/portfolio';
 
 const nextConfig = {
   reactStrictMode: true,
